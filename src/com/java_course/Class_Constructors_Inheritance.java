ee2c2c90736b2970313b8b39ea3478387591ebc5
Osmanjan Omar <hizmatla@gmail.com>
2018-12-18 12:34:41 -0800

    ,,,

--- src/com/java_course/Class_Constructors_Inheritance.java
+++ src/com/java_course/Class_Constructors_Inheritance.java
@@ -1,5 +1,24 @@
 package com.java_course;
 
+
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class Class_Constructors_Inheritance {
 
+    ArrayList<String> groceryList = new ArrayList<String>();
+
+    private LinkedList<String> linkedListString = new LinkedList<String>();
+
+    public Class_Constructors_Inheritance(ArrayList<String> groceryList, LinkedList<String> linkedListString) {
+        this.groceryList = groceryList;
+        this.linkedListString = linkedListString;
+    }
+
+    public void test() {
+        linkedListString.push("Osmanjan");
+        linkedListString.pop();
+
+    }
 }
