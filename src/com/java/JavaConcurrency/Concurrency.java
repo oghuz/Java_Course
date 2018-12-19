2b5d8fda885e3341d2b01d30d97eabb965955d1d
Osmanjan Omar <hizmatla@gmail.com>
2018-12-19 12:31:54 -0800

    created colors to mark up debugging prints

--- src/com/java/JavaConcurrency/Concurrency.java
+++ src/com/java/JavaConcurrency/Concurrency.java
@@ -2,12 +2,9 @@ package com.java.JavaConcurrency;
 
 // Basics of Java concurrency 
 
-class ConcurrencyJava extends Thread{
+class ConcurrencyJava{
     public static void main(String[] args) {
         AnotherThread aThread = new AnotherThread();
         aThread.start();
     }
-
-    
-    
 }
