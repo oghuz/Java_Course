867ea4027b135b8aa469ff6f839b76bec29d3bec
Osmanjan Omar <hizmatla@gmail.com>
2019-01-01 12:28:29 -0800

    used ending with sign

--- src/com/java/RegularExpressions/RexMain.java
+++ src/com/java/RegularExpressions/RexMain.java
@@ -8,6 +8,11 @@ public class RexMain {
         String alphaNumeric = "abcDeeefF123Ghg@ghkhk,JKo90898Y.incskjb";
 
         System.out.println(alphaNumeric.replaceAll("^abcD", "Ttt"));
+        System.out.println(alphaNumeric.replaceAll("jb$", "ended"));
+        System.out.println(alphaNumeric.replaceAll("[k]", "QQQQ"));
+        System.out.println(alphaNumeric.replaceAll("[ee]", " I replaced an ee here"));
+        System.out.println(alphaNumeric.replaceAll("[k][h]", " I replaced k here followed by h"));
+
 
 
     }
