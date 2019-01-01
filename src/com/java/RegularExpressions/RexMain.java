0b9c68921f66400cbd50298792f43ad1b206268c
Osmanjan Omar <hizmatla@gmail.com>
2019-01-01 12:51:15 -0800

    RegEx continues

--- src/com/java/RegularExpressions/RexMain.java
+++ src/com/java/RegularExpressions/RexMain.java
@@ -13,6 +13,37 @@ public class RexMain {
         System.out.println(alphaNumeric.replaceAll("[ee]", " I replaced an ee here"));
         System.out.println(alphaNumeric.replaceAll("[k][h]", " I replaced k here followed by h"));
 
+        String newAlphaNumeric = "abcDeeefF123Ghg@ghkhk,JKo90898Y.incskjb";
+        System.out.println(newAlphaNumeric.replaceAll("[^ef]", "O"));
+        System.out.println(newAlphaNumeric.replaceAll("[a-g2-9]", " &&"));
+        System.out.println(newAlphaNumeric.replaceAll("(?i)[a-g2-9]", " 66"));
+        System.out.println(newAlphaNumeric.replaceAll("[0-9]", " 77"));
+        System.out.println(newAlphaNumeric.replaceAll("\\d", " 77")); // all digits
+        System.out.println(newAlphaNumeric.replaceAll("\\D", " 222")); // all non digits
+
+        System.out.println("==================================");
+        String withSpaceAndTabs = " A String with space and\t and also a newLine\n";
+        System.out.println(withSpaceAndTabs);
+        System.out.println("==================================");
+        System.out.println(withSpaceAndTabs.replaceAll("\\s", "SPACE"));
+        System.out.println(withSpaceAndTabs.replaceAll("\\S", "SPACE"));
+        System.out.println(withSpaceAndTabs.replaceAll("\\t", "TAB"));
+       // System.out.println(withSpaceAndTabs.replaceAll("\\T", "TAB")); illegal operation
+        System.out.println(withSpaceAndTabs.replaceAll("\\w", "WHITE"));
+        System.out.println(withSpaceAndTabs.replaceAll("\\W", "WHITE"));
+        System.out.println(withSpaceAndTabs.replaceAll("\\b", "^"));
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     }
