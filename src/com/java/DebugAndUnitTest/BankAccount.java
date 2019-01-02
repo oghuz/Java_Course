1a0372b57343680d4249eeed5f5e2d97cc8663f8
Osmanjan Omar <hizmatla@gmail.com>
2019-01-02 11:56:53 -0800

    fixed testing class

---
+++ src/com/java/DebugAndUnitTest/BankAccount.java
@@ -0,0 +1,31 @@
+package com.java.DebugAndUnitTest;
+
+public class BankAccount {
+    private String firstName;
+    private String lastName;
+    private double balance;
+
+    public BankAccount(String firstName, String lastName, double balance) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.balance = balance;
+    }
+
+    // the branch argument is true if the customer is performing the transaction
+    // at the branch, with a teller
+    // It's false if the customer is performing the transaction at an ATM
+
+    public double deposit(double amount, boolean branch) {
+        balance += amount;
+        return balance;
+    }
+    public double withdraw(double amount, boolean branch) {
+        balance =- amount;
+        return balance;
+    }
+
+    public double getBalance(){
+        return balance;
+    }
+
+}
---
+++ src/com/java/DebugAndUnitTest/BankAccountTest.java
@@ -0,0 +1,23 @@
+package com.java.DebugAndUnitTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BankAccountTest {
+
+    @org.junit.jupiter.api.Test
+    void deposit() {
+        fail(" this test will fail");
+    }
+
+    @org.junit.jupiter.api.Test
+    void withdraw() {
+        fail(" this test will fail");
+    }
+
+    @org.junit.jupiter.api.Test
+    void getBalance() {
+        fail(" this test will fail");
+    }
+
+
+}
