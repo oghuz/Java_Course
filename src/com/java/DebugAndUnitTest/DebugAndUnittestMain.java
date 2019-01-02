bd39cb9f1e3b40609c3317b04ef7f287ca8b4b55
Osmanjan Omar <hizmatla@gmail.com>
2019-01-01 18:53:38 -0800

    started package for debug and unit test

---
+++ src/com/java/DebugAndUnitTest/DebugAndUnittestMain.java
@@ -0,0 +1,13 @@
+package com.java.DebugAndUnitTest;
+
+public class DebugAndUnittestMain {
+    public static void main(String[] args) {
+        StringUtilities utils = new StringUtilities();
+        StringBuilder sb = new StringBuilder();
+
+        while(sb.length() < 10) {
+            utils.addChar(sb, 'a');
+        }
+        System.out.println(sb);
+    }
+}
---
+++ src/com/java/DebugAndUnitTest/StringUtilities.java
@@ -0,0 +1,10 @@
+package com.java.DebugAndUnitTest;
+
+public class StringUtilities {
+    private StringBuilder sBuilder = new StringBuilder();
+    private int charAdded = 0;
+    public void addChar(StringBuilder sBuilder, char c) {
+        this.sBuilder.append(c);
+        charAdded++;
+    }
+}
