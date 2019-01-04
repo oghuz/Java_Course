65d08e4ba91ad88aa9d311be2b70bea1c1c44ee1
Osmanjan Omar <hizmatla@gmail.com>
2019-01-03 21:18:54 -0800

    created data base and table inside java programm

--- src/com/java/DataBaseTest.java
+++ src/com/java/DataBaseTest.java
@@ -3,13 +3,22 @@ package com.java;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DataBaseTest {
 
     public static void main(String[] args) {
-        try{
+//        try(Connection conn = DriverManager.getConnection("jdbc:sqlite:/Users/osmanjan/work/Java/Java_course/Java_Course/TestDB.db");
+//            Statement statement = conn.createStatement()){
+//            statement.execute("create table contacts (name TEXT, email TEXT, phone INTEGER)");
+//        }
+        try {
             Connection conn = DriverManager.getConnection("jdbc:sqlite:/Users/osmanjan/work/Java/Java_course/Java_Course/TestDB.db");
-        }catch(SQLException e){
+            Statement statement = conn.createStatement();
+            statement.execute("create table contacts (name TEXT, email TEXT, phone INTEGER)");
+            conn.close();
+            statement.close();
+        } catch (SQLException e) {
             System.out.printf("SQL data base table could not be created, info : %s%n", e.getLocalizedMessage());
         }
     }
